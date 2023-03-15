06cc019d52c909070c34f2965fa9abc72f3052d7
Pawan <salvepawan3@gmail.com>
2023-03-15 09:34:23 +0530

    Photos change

--- src/components/Layout/MainHeader.js
+++ src/components/Layout/MainHeader.js
@@ -1,10 +1,12 @@
 import CartButton from '../Cart/CartButton';
 import classes from './MainHeader.module.css';
-
+import winwin from '../../assests/win.jpg'
 const MainHeader = (props) => {
   return (
     <header className={classes.header}>
-      <h1>ReduxCart</h1>
+    <img style={{width:'20%', height:'90%'}}src={winwin} alt='A table full of delicious food!' />  
+
+      {/* <h1>ReduxCart</h1> */}
       <nav>
         <ul>
           <li>
